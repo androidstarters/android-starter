c38df749f63379f38c859bee01be9e5484a67e44
Shivam Chopra <makewaysforshivam@gmail.com>
2017-05-30 16:51:55 +0530

    Removed MvpStarterNetworkFactory

--- app/src/main/java/io/mvpstarter/sample/di/module/AppModule.java
+++ app/src/main/java/io/mvpstarter/sample/di/module/AppModule.java
@@ -4,28 +4,18 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import javax.inject.Singleton;
-
 import dagger.Module;
 import dagger.Provides;
-import io.mvpstarter.sample.data.remote.MvpStarterServiceFactory;
-import io.mvpstarter.sample.data.remote.PokemonApi;
 import io.mvpstarter.sample.di.ApplicationContext;
 
 import static io.mvpstarter.sample.common.constants.PrefConstants.PREF_FILE_NAME;
 
-@Module
+@Module(includes = {ApiModule.class})
 public class AppModule {
     private final Application application;
 
     public AppModule(Application application) {
         this.application = application;
-    }
-
-    @Provides
-    @Singleton
-    static PokemonApi provideMvpStarterService() {
-        return MvpStarterServiceFactory.makeStarterService();
     }
 
     @Provides
