2443c6e76a568869045fce57b2e6148a675c16ac
Shivam Chopra <makewaysforshivam@gmail.com>
2017-07-02 09:48:54 +0530

    Add 3 new libs : 1.) Chuck --> Which helps in tracking network requests on device itself. 2.) Traceur --> Easier RxJava2 debugging with better stacktraces 3.) SherlockSherlock --> Sherlock reports any crash that occurs in your application

--- app/src/main/java/io/mvpstarter/sample/MvpStarterApplication.java
+++ app/src/main/java/io/mvpstarter/sample/MvpStarterApplication.java
@@ -4,16 +4,19 @@ import android.app.Application;
 import android.content.Context;
 
 import com.facebook.stetho.Stetho;
+import com.singhajit.sherlock.core.Sherlock;
 import com.squareup.leakcanary.LeakCanary;
+import com.tspoon.traceur.Traceur;
 
 import io.mvpstarter.sample.injection.component.AppComponent;
 import io.mvpstarter.sample.injection.component.DaggerAppComponent;
 import io.mvpstarter.sample.injection.module.AppModule;
+import io.mvpstarter.sample.injection.module.NetworkModule;
 import timber.log.Timber;
 
 public class MvpStarterApplication extends Application {
 
-    AppComponent appComponent;
+    private AppComponent appComponent;
 
     public static MvpStarterApplication get(Context context) {
         return (MvpStarterApplication) context.getApplicationContext();
@@ -27,12 +30,17 @@ public class MvpStarterApplication extends Application {
             Timber.plant(new Timber.DebugTree());
             Stetho.initializeWithDefaults(this);
             LeakCanary.install(this);
+            Sherlock.init(this);
+            Traceur.enableLogging();
         }
     }
 
     public AppComponent getComponent() {
         if (appComponent == null) {
-            appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
+            appComponent = DaggerAppComponent.builder()
+                    .networkModule(new NetworkModule(this))
+                    .appModule(new AppModule(this))
+                    .build();
         }
         return appComponent;
     }
